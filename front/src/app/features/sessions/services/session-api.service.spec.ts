9bbfa08c33246d4b2a48a344ab2117b62b336655
arthurgcliking <arthur.gumery@cliking.co>
2024-09-14 10:51:56 +0200

    fixed coverage for back and front

--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -1,22 +1,63 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { expect } from '@jest/globals';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SessionApiService } from './session-api.service';
+import { Session } from '../interfaces/session.interface';
+import { expect as jestExpect } from '@jest/globals';
 
-describe('SessionsService', () => {
-  let service: SessionApiService;
+describe('SessionApiService Unit Tests', function() {
+  let sessionApiSvc: SessionApiService;
+  let httpCtrl: HttpTestingController;
+
+  const mockSession: Session = {
+    name: '',
+    description: '',
+    date: new Date(),
+    teacher_id: 0,
+    users: [],
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [SessionApiService]
     });
-    service = TestBed.inject(SessionApiService);
+
+    sessionApiSvc = TestBed.inject(SessionApiService);
+    httpCtrl = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpCtrl.verify();
+  });
+
+  it('should create a new Session', function() {
+    sessionApiSvc.create(mockSession).subscribe((response) => {
+      jestExpect(response).toStrictEqual(mockSession);
+    });
+
+    const request = httpCtrl.expectOne('api/session');
+    jestExpect(request.request.method).toBe('POST');
+    request.flush(mockSession);
+  });
+
+  it('should update an existing Session', function() {
+    const sessionId = '1';
+
+    sessionApiSvc.update(sessionId, mockSession).subscribe((response) => {
+      jestExpect(response).toStrictEqual(mockSession);
+    });
+
+    const request = httpCtrl.expectOne(`api/session/${sessionId}`);
+    jestExpect(request.request.method).toBe('PUT');
+    request.flush(mockSession);
+  });
+
+  it('should delete a Session', function() {
+    const sessionId = '1';
+
+    sessionApiSvc.delete(sessionId).subscribe();
+
+    const request = httpCtrl.expectOne(`api/session/${sessionId}`);
+    jestExpect(request.request.method).toBe('DELETE');
   });
 });
